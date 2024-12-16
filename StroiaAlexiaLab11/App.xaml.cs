ef30eaafbf96ffcf3b2a655e0e2446a3de4dbc2f
alexiastroia9 <alexiastroia9@gmail.com>
2024-12-16 18:37:47 +0200

    Add project files.

---
+++ StroiaAlexiaLab11/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using StroiaAlexiaLab11.Data;
+using StroiaAlexiaLab11.Models;
+namespace StroiaAlexiaLab11
+{
+    public partial class App : Application
+    {
+        public static ShoppingListDatabase Database { get; private set; }
+        public App()
+        {
+            Database = new ShoppingListDatabase(new RestService());
+            MainPage = new NavigationPage(new ListEntryPage());
+        }
+    }
+}
---
+++ StroiaAlexiaLab11/ListPage.xaml.cs
@@ -0,0 +1,25 @@
+using StroiaAlexiaLab11.Data;
+using StroiaAlexiaLab11.Models;
+namespace StroiaAlexiaLab11;
+
+public partial class ListPage : ContentPage
+{
+	public ListPage()
+	{
+		InitializeComponent();
+	}
+    async void OnSaveButtonClicked(object sender, EventArgs e)
+    {
+        var slist = (ShopList)BindingContext;
+        slist.Date = DateTime.UtcNow;
+        await App.Database.SaveShopListAsync(slist);
+        await Navigation.PopAsync();
+    }
+    async void OnDeleteButtonClicked(object sender, EventArgs e)
+    {
+        var slist = (ShopList)BindingContext;
+        await App.Database.DeleteShopListAsync(slist);
+        await Navigation.PopAsync();
+    }
+
+}
